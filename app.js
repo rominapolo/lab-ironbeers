ca8ce06bcb23cff8a20336ffa3407009fd9eb8a1
Romina Polo <rominalpolo@gmail.com>
2022-09-03 17:58:47 -0400

    lab without styling

--- app.js
+++ app.js
@@ -14,12 +14,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers().then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi)
+    const data = {
+      content: beersFromApi
+    }
+    res.render('beers',data)
+
+  }).catch(error => console.log(error));
+})
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom().then(responsefromApi => {
+    console.log('response from database: ', responsefromApi)
+
+    const data = {
+      content: responsefromApi
+    }
+    res.render('random-beer',data)
+
+  }).catch(error => console.log(error));
+})
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
